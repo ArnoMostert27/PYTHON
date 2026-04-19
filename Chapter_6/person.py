869e2f6040280f1f08ba6be84804c91cac2fbbf4
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 22:48:41 +0200

    Add files via upload

---
+++ Chapter_6/person.py
@@ -0,0 +1,13 @@
+# 6-1 Person Dictionary
+
+person = {
+    "first_name": "Arno",
+    "last_name": "Mostert",
+    "age": 20,
+    "city": "Pretoria"
+}
+
+print(person["first_name"])
+print(person["last_name"])
+print(person["age"])
+print(person["city"])
