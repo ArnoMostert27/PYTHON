27c97a4058a77a6db622d07dd7ff1cadbcd0cdbb
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 22:18:41 +0200

    Add files via upload

---
+++ Chapter_3/transport.py
@@ -0,0 +1,7 @@
+# 3-3 Your Own List
+
+transport = ["Honda motorcycle", "BMW car", "Tesla"]
+
+print(f"I would like to own a {transport[0]}")
+print(f"I would like to own a {transport[1]}")
+print(f"I would like to own a {transport[2]}")
