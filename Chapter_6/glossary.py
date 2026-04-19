f6af2cc9699559a3a486d5862d42f57f398c1aa9
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 22:47:43 +0200

    Add files via upload

---
+++ Chapter_6/glossary.py
@@ -0,0 +1,12 @@
+# 6-3 Glossary
+
+glossary = {
+    "variable": "A container for storing data values",
+    "loop": "A way to repeat code multiple times",
+    "list": "A collection of items in order",
+    "dictionary": "A collection of key-value pairs",
+    "function": "A block of reusable code"
+}
+
+for word, meaning in glossary.items():
+    print(f"{word}:\n  {meaning}\n")
