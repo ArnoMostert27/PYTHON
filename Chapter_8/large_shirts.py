eb276405002f56f55446dbffce1112878b96f187
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 22:57:57 +0200

    Add files via upload

---
+++ Chapter_8/large_shirts.py
@@ -0,0 +1,13 @@
+# 8-4 Large Shirts (default values)
+
+def make_shirt(size="L", message="I love Python"):
+    print(f"Size: {size}, Message: {message}")
+
+# Large shirt (default)
+make_shirt()
+
+# Medium shirt (default message)
+make_shirt("M")
+
+# Custom shirt
+make_shirt("S", "Coding is awesome")
