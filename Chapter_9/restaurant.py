49683c13c5d6764926c034b61bd715595f1de1ce
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 23:01:33 +0200

    Add files via upload

---
+++ Chapter_9/restaurant.py
@@ -0,0 +1,20 @@
+# 9-1 Restaurant Class
+
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name} serves {self.cuisine_type} food.")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open!")
+
+restaurant = Restaurant("Pizza Palace", "Italian")
+
+print(restaurant.restaurant_name)
+print(restaurant.cuisine_type)
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
