3b4fe3f1e9be41ae21690bcb329f080d15d73d9b
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 22:17:24 +0200

    Add files via upload

---
+++ Chapter_3/guest_list.py
@@ -0,0 +1,7 @@
+# 3-4 Guest List
+
+guests = ["Elon Musk", "Albert Einstein", "Steve Jobs"]
+
+print(f"Dear {guests[0]}, you are invited to dinner.")
+print(f"Dear {guests[1]}, you are invited to dinner.")
+print(f"Dear {guests[2]}, you are invited to dinner.")
