014f694af8066a7318884ed072aa4ceebb82fcc0
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 22:57:25 +0200

    Add files via upload

---
+++ Chapter_8/cities.py
@@ -0,0 +1,8 @@
+# 8-5 Cities
+
+def describe_city(city, country="South Africa"):
+    print(f"{city} is in {country}")
+
+describe_city("Pretoria")
+describe_city("Cape Town")
+describe_city("Tokyo", "Japan")
