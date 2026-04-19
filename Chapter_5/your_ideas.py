0f673a7530d9aeda6c5c7fda2c21a5569da36b2c
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 22:45:20 +0200

    Add files via upload

---
+++ Chapter_5/your_ideas.py
@@ -0,0 +1,6 @@
+# Ideas for future projects
+
+print("Game Idea: Zombie survival game (DayZ-style)")
+print("Web App Idea: Beauty product store (like your ecommerce project)")
+print("Data Idea: Analyze YouTube growth stats")
+print("Tool Idea: Worship setlist planner")
