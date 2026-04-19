574527cc6ab860eddcc10a427c9af5cedcbbdb3f
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 22:15:43 +0200

    Add files via upload

---
+++ Chapter_3/changing_guest_list.py
@@ -0,0 +1,11 @@
+# 3-5 Changing Guest List
+
+guests = ["Elon Musk", "Albert Einstein", "Steve Jobs"]
+
+print("Steve Jobs can't make it.")
+
+guests[2] = "Bill Gates"
+
+print(f"Dear {guests[0]}, you are invited to dinner.")
+print(f"Dear {guests[1]}, you are invited to dinner.")
+print(f"Dear {guests[2]}, you are invited to dinner.")
