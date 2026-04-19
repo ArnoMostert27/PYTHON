c9160fab9fd0c27a9c25575959db48216fdd16b0
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 22:50:36 +0200

    Add files via upload

---
+++ Chapter_7/movie_tickets.py
@@ -0,0 +1,16 @@
+# 7-5 Movie Tickets
+
+while True:
+    age = input("Enter your age (or 'quit' to stop): ")
+
+    if age == "quit":
+        break
+
+    age = int(age)
+
+    if age < 3:
+        print("Your ticket is free.")
+    elif age <= 12:
+        print("Your ticket is $10.")
+    else:
+        print("Your ticket is $15.")
