716b1f8f783673b717993b39624aba551a1392f0
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 22:17:00 +0200

    Add files via upload

---
+++ Chapter_3/greetings.py
@@ -0,0 +1,7 @@
+# 3-2 Greetings
+
+names = ["Alex", "John", "Sarah"]
+
+print(f"Hello {names[0]}, hope you're well!")
+print(f"Hello {names[1]}, hope you're well!")
+print(f"Hello {names[2]}, hope you're well!")
