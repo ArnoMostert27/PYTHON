4f98915bd6ec003ad392acf628d792a41c0a3f22
Arno Mostert <9qkk4vj8h6@privaterelay.appleid.com>
2026-04-19 22:47:27 +0200

    Add files via upload

---
+++ Chapter_6/favorite_numbers.py
@@ -0,0 +1,15 @@
+# 6-2 Favorite Numbers
+
+favorite_numbers = {
+    "Alex": 7,
+    "John": 3,
+    "Sarah": 12,
+    "Mike": 9,
+    "Emma": 5
+}
+
+print(f"Alex's favorite number is {favorite_numbers['Alex']}")
+print(f"John's favorite number is {favorite_numbers['John']}")
+print(f"Sarah's favorite number is {favorite_numbers['Sarah']}")
+print(f"Mike's favorite number is {favorite_numbers['Mike']}")
+print(f"Emma's favorite number is {favorite_numbers['Emma']}")
